1d977fc78c0bec74bd92f837e00566adc0b98369
Jamanul <jk.skb.25@gmail.com>
2024-09-07 21:40:28 +0600

    fixed height issue

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -8,13 +8,15 @@ import Link from 'next/link';
 const page = () => {
 
     return (
-        <div className='flex flex-col md:flex-row '>
-            <div className='relative'>
-            <Image src={'https://i.ibb.co.com/VJZvGn1/dbe7d1e0-4e42-4325-9d76-c38d558a3c06.jpg'} height={628} width={688} alt='shoe.png'/>
+        <div className='flex flex-col h-screen md:flex-row '>
+            <div className='relative flex-1'>
+            <div className='relative w-full h-screen'>
+            <Image src={'https://i.ibb.co.com/VJZvGn1/dbe7d1e0-4e42-4325-9d76-c38d558a3c06.jpg'} layout='fill' alt='shoe.png'/>
+            </div>
             <Link href={'login'}><button className='absolute top-96 right-0 rounded-l-full text-2xl font-semibold px-8 py-4 bg-gray-400 text-blue-950'>LOGIN</button></Link>
             <Link href={'signup'}><button className='absolute top-[31rem] right-0 rounded-l-full text-2xl font-semibold px-5 py-4  text-white'>SIGN UP</button></Link>
             </div>
-            <div className='flex flex-col items-center justify-center mx-auto'>
+            <div className='flex flex-col flex-1 items-center justify-center mx-auto'>
                 <Image src={'https://i.ibb.co.com/VJV2GRy/images.png'} height={100} width={150} alt='logo.png'/>
                 <form className='mt-12' action="">
                     <div className='flex gap-1 items-center border-b-2 pb-1 mb-8 pr-12 border-secondary'>   
@@ -29,7 +31,7 @@ const page = () => {
                    <input type="submit" className='bg-primary text-white px-6 text-sm  py-1 rounded-full' value="LOGIN"  />
                    </div>
                 </form>
-                <div className="divider text-xs font-medium text-secondary">OR LOGIN WITH</div>
+                <div className="divider text-xs font-medium text-secondary md:mx-36">OR LOGIN WITH</div>
                 <div className='flex items-center justify-between gap-2'>
                     <div className='bg-primary text-white px-6 text-sm font-medium py-1 rounded-full'>Google</div>
                     <div className='bg-primary text-white px-6 text-sm font-medium py-1 rounded-full'>Facebook</div>
