028edc18a3ca5ccd67e7a0e87cddfdd3b61ed39b
Jamanul <jk.skb.25@gmail.com>
2024-09-09 14:25:13 +0600

    shoecard api

--- src/app/AllShoe/api/get-all/route.ts
+++ src/app/AllShoe/api/get-all/route.ts
@@ -1,9 +1,13 @@
-import { connectDb } from "@/app/lib/connectDb"
+import { connectDb } from '@/app/lib/connectDb';
 
-export const GET = async ()=>{
-    const db = await connectDb()
-    const shoesCollection = db?.collection('shoes')
-    const shoes = await shoesCollection?.find().toArray()
-    console.log(shoes)
-    return Response.json({shoes})
-}
+export const GET = async () => {
+  try {
+    const db = await connectDb();
+    const shoesCollection = db?.collection('shoes');
+    const shoes = shoesCollection?.find();
+    console.log(shoes);
+    return Response.json({ shoes });
+  } catch (error) {
+    console.log(error);
+  }
+};
--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,10 +1,12 @@
 import Banner from './Banner';
+import ShoeSection from './ShoeSection';
 import ShopNow from './ShopNow';
 
 const HomePage = () => {
   return (
     <div className=''>
       <Banner />
+      <ShoeSection/>
       <ShopNow />
     </div>
   );
