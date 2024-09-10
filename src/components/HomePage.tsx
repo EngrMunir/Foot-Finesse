b04cd86ab893f075a9fc9ebd7aa93ea95f603b94
Jamanul <jk.skb.25@gmail.com>
2024-09-10 15:57:29 +0600

    to avoid marging

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,18 +1,15 @@
 import Banner from './Banner';
-<<<<<<< HEAD
-import ShoeSection from './ShoeSection';
-=======
 import LatestCollection from './LatestCollection';
->>>>>>> e96977f4f19ced450e434b55cd01d18ffbfa6a42
+
 import ShopNow from './ShopNow';
 
 const HomePage = () => {
   return (
     <div className=''>
       <Banner />
-      <ShoeSection/>
+      
       <ShopNow />
-      <LatestCollection />
+      <LatestCollection/>
     </div>
   );
 };
