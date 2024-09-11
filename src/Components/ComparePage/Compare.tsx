850d9c8485bd917a813fab7a2481823c5284b240
FARUK2059 <omarfaruk34552@gmail.com>
2024-09-11 13:41:17 +0600

    Compare page Update

--- src/Components/ComparePage/Compare.tsx
+++ src/Components/ComparePage/Compare.tsx
@@ -1,5 +1,9 @@
 
-
+import Image from "next/image";
+import { FaStar } from "react-icons/fa";
+import { FaStarHalfStroke } from "react-icons/fa6";
+import comImage from "../../../public/AboutUs/Image/pic1.jpg"
+import comImage2 from "../../../public/AboutUs/Image/pic2.jpg"
 
 const Compare = () => {
     return (
@@ -18,22 +22,26 @@ const Compare = () => {
                     <tbody className="divide-y divide-gray-200">
 
                         <tr className="odd:bg-gray-50">
-                            <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Image</td>
+                            <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900"></td>
                             <td className="whitespace-nowrap px-4 py-2 text-gray-700">
                                 <a href="#" className="block rounded-lg p-4 shadow-sm shadow-indigo-100">
-                                    <img
+                                    <Image
+                                        width={400}
+                                        height={400}
                                         alt=""
-                                        src="https://i.ibb.co.com/Lx6nXWg/d24128e58f49b52a981ec79a3476ef7d.jpg"
+                                        src={comImage}
                                         className="h-56 w-56l rounded-md object-cover"
                                     />
                                 </a>
                             </td>
                             <td className="whitespace-nowrap px-4 py-2 text-gray-700">
                                 <a href="#" className="block rounded-lg p-4 shadow-sm shadow-indigo-100">
-                                    <img
+                                    <Image
+                                        width={400}
+                                        height={400}
                                         alt=""
-                                        src="https://i.ibb.co.com/DphWjx4/a3bf40693736d5e57b042fe56594aafa.jpg"
-                                        className="h-56 w-56 rounded-md object-cover"
+                                        src={comImage2}
+                                        className="h-56 w-56l rounded-md object-cover"
                                     />
                                 </a>
                             </td>
@@ -45,7 +53,7 @@ const Compare = () => {
                                 Playground Runner
                             </td>
                             <td className="whitespace-nowrap px-4 py-2 text-gray-700">
-                                Playground Runner
+                                Tiny Trek Sneakers
                             </td>
                         </tr>
                         <tr className="odd:bg-gray-50">
@@ -54,7 +62,7 @@ const Compare = () => {
                                 Man Shoes
                             </td>
                             <td className="whitespace-nowrap px-4 py-2 text-gray-700">
-                            Man Shoes
+                                Man Shoes
                             </td>
                         </tr>
 
@@ -65,6 +73,15 @@ const Compare = () => {
                             </td>
                             <td className="whitespace-nowrap px-4 py-2 text-gray-700">
                                 30 $
+                            </td>
+                        </tr>
+                        <tr className="odd:bg-gray-50">
+                            <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Color</td>
+                            <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                                White, Black
+                            </td>
+                            <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                                White, Black
                             </td>
                         </tr>
                         <tr className="odd:bg-gray-50">
@@ -80,10 +97,22 @@ const Compare = () => {
                         <tr className="odd:bg-gray-50">
                             <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Rating</td>
                             <td className="whitespace-nowrap px-4 py-2 text-gray-700">
-                                4.5
+                                4.5 <span className="flex gap-2">
+                                    <FaStar />
+                                    <FaStar />
+                                    <FaStar />
+                                    <FaStar />
+                                    <FaStarHalfStroke />
+                                </span>
                             </td>
                             <td className="whitespace-nowrap px-4 py-2 text-gray-700">
-                                4.6
+                                4.6 <span className="flex gap-2">
+                                    <FaStar />
+                                    <FaStar />
+                                    <FaStar />
+                                    <FaStar />
+                                    <FaStarHalfStroke />
+                                </span>
                             </td>
                         </tr>
 
@@ -94,6 +123,83 @@ const Compare = () => {
                             </td>
                             <td className="whitespace-nowrap px-4 py-2 text-gray-700">
                                 Durable shoes designed for everyday play
+                            </td>
+                        </tr>
+                        <tr className="odd:bg-gray-50">
+                            <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Size</td>
+                            <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                                All size avelable
+                            </td>
+                            <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                                All size avelable
+                            </td>
+                        </tr>
+                        <tr className="odd:bg-gray-50">
+                            <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Company Name</td>
+                            <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                                Lotto
+                            </td>
+                            <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                                Appex
+                            </td>
+                        </tr>
+                        <tr className="odd:bg-gray-50">
+                            <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Launch</td>
+                            <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                                12/07/2024
+                            </td>
+                            <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                                18/07/2024
+                            </td>
+                        </tr>
+                        <tr className="odd:bg-gray-50">
+                            <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Stock</td>
+                            <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                                Avalable
+                            </td>
+                            <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                                Avalable
+                            </td>
+                        </tr>
+                        <tr className="odd:bg-gray-50">
+                            <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900"></td>
+                            <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                                {/* Button  */}
+                                <div className="justify-center items-center text-center grid">
+                                    <a
+                                        className="group relative inline-block  overflow-hidden border border-indigo-600 px-8 py-3 focus:outline-none focus:ring "
+                                        href="#"
+                                    >
+                                        <span
+                                            className="absolute inset-y-0 left-0 w-[2px] bg-indigo-600 transition-all group-hover:w-full group-active:bg-indigo-500"
+                                        ></span>
+
+                                        <span
+                                            className="relative text-sm font-medium text-indigo-600 transition-colors group-hover:text-white"
+                                        >
+                                            Add to Card
+                                        </span>
+                                    </a>
+                                </div>
+                            </td>
+                            <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                                {/* Button  */}
+                                <div className="justify-center items-center text-center grid">
+                                    <a
+                                        className="group relative inline-block  overflow-hidden border border-indigo-600 px-8 py-3 focus:outline-none focus:ring "
+                                        href="#"
+                                    >
+                                        <span
+                                            className="absolute inset-y-0 left-0 w-[2px] bg-indigo-600 transition-all group-hover:w-full group-active:bg-indigo-500"
+                                        ></span>
+
+                                        <span
+                                            className="relative text-sm font-medium text-indigo-600 transition-colors group-hover:text-white"
+                                        >
+                                            Add to Card
+                                        </span>
+                                    </a>
+                                </div>
                             </td>
                         </tr>
 
