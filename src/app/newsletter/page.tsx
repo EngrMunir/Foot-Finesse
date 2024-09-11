c0b0b3e9f501e2e7033773f53dc1430cb00ef9ad
FARUK2059 <omarfaruk34552@gmail.com>
2024-09-12 03:00:47 +0600

    newsletter Design Complet

---
+++ src/app/newsletter/page.tsx
@@ -0,0 +1,13 @@
+import NewsLetter from "@/Components/NewsLetter/NewsLetter";
+
+
+
+const page = () => {
+    return (
+        <div>
+            <NewsLetter></NewsLetter>
+        </div>
+    );
+};
+
+export default page;
