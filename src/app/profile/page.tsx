6f861b3d3b7afd67b6be8ed1cf01e50734ff4ce8
Jamanul <jk.skb.25@gmail.com>
2024-09-11 14:12:29 +0600

    added form to update info of profile

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,9 +3,18 @@ import axios from 'axios';
 import { useSession } from 'next-auth/react';
 import Image from 'next/image';
 import { redirect } from 'next/navigation';
-import React from 'react';
+import React, { useState } from 'react';
 
 const page = () => {
+    const [fullName,setFullName]=useState<string>('')
+    const [zip,setZip]=useState<string>('')
+    const [city,setCity]=useState<string>('')
+    const [country,setCountry]=useState<string>('')
+    const [street,setStreet]=useState<string>('')
+    const [phoneNumber,setPhoneNumber]=useState<string>('')
+    const handleEditInfo =()=>{
+      console.log('name')
+    }
     const imageHostingKey = process.env.NEXT_PUBLIC_IMAGE_API
     const imageHostingApi =`https://api.imgbb.com/1/upload?key=${imageHostingKey}`
   const session = useSession();
@@ -21,7 +30,7 @@ const page = () => {
       }
     })
     console.log(res.data)
-    
+
   }
   return (
     <div className='container mx-auto'>
@@ -36,7 +45,8 @@ const page = () => {
         />
         <p className='absolute left-[15%] top-[38%] text-6xl font-bold text-white'>Profile</p>
       </div>
-      <div className='mx-auto mt-10 max-w-4xl'>
+     <div className="flex flex-col mx-auto mt-10 max-w-4xl md:flex-row gap-6">
+     <div className=''>
           <div className='relative h-36 w-36'>
             {session?.data?.user?.image ? (
               <Image
@@ -61,6 +71,90 @@ const page = () => {
             </label>
           </button>
       </div>
+      <form
+            onSubmit={handleEditInfo}
+            action=""
+            className="flex-1"
+          >
+            <div className="form-control">
+              <input
+                type="text"
+                placeholder="First and Last name"
+                name="name"
+                value={fullName}
+                className="input input-bordered max-w-sm"
+                onChange={(e) => setFullName(e.target.value)}
+                required
+              />
+            </div>
+            <div className="form-control">
+              <input
+                type="text"
+                placeholder=""
+                name="name"
+                value={session.data?.user?.email}
+                className="input input-bordered mt-2 bg-slate-300 max-w-sm"
+                readOnly
+                required
+              />
+            </div>
+            <div className="form-control">
+              <input
+                type="number"
+                placeholder="Your phone Number"
+                name="phoneNumber"
+                value={phoneNumber}
+                className="input input-bordered mt-2 max-w-sm"
+                onChange={(e) => setPhoneNumber(e.target.value)}
+              />
+            </div>
+            <div className="form-control">
+              <input
+                type="text"
+                placeholder="Street"
+                name="street"
+                value={street}
+                className="input input-bordered mt-2 max-w-sm"
+                onChange={(e) => setStreet(e.target.value)}
+              />
+            </div>
+            <div className="flex flex-col"> 
+            <div className="form-control ">
+              <input
+                type="number"
+                placeholder="zip"
+                name="zip"
+                value={zip}
+                className="input input-bordered mt-2 max-w-sm"
+                onChange={(e) => setZip(e.target.value)}
+              />
+            </div>
+            <div className="form-control">
+              <input
+                type="text"
+                placeholder="city"
+                name="city"
+                value={city}
+                className="input input-bordered mt-2 max-w-sm"
+                onChange={(e) => setCity(e.target.value)}
+              />
+            </div>
+            </div>
+            <div className="form-control">
+              <input
+                type="text"
+                placeholder="country"
+                name="country"
+                value={country}
+                className="input input-bordered mt-2 max-w-sm"
+                onChange={(e) => setCountry(e.target.value)}
+              />
+            </div>
+            <div className="form-control mt-2">
+              <button className="btn bg-primary text-white max-w-sm">Save</button>
+            </div>
+          </form>
+     </div>
     </div>
   );
 };
