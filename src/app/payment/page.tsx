e425a0785c0603c4690086e54ecbc79a28bf37bd
dev-ahad-ali <ahadali.dev@gmail.com>
2024-09-14 21:02:25 +0600

    Update : payment system is connected with cart

--- src/app/payment/page.tsx
+++ src/app/payment/page.tsx
@@ -1,16 +1,19 @@
 'use client';
 import CheckoutPage from '@/components/CheckoutPage';
 import { convertToSubCurrency } from '@/lib/convertTosubCurency';
+import { CartContext, CartContextType } from '@/providers/CartProvider';
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
+import { useContext } from 'react';
 
 if (process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY === undefined) {
   throw new Error('STRIPE_PUBLISHER_KEY is undefined');
 }
 const stipePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
 
-function page() {
-  const amount = 49.99; // this will be dynamic
+function Page() {
+  const { grandTotal } = useContext(CartContext) as CartContextType;
+  const amount = grandTotal || 0.6; // this will be dynamic
   return (
     <section className='grid min-h-screen place-items-center'>
       <div className='max-w-6xl rounded-lg border p-4 shadow-xl'>
@@ -32,4 +35,4 @@ function page() {
   );
 }
 
-export default page;
+export default Page;
