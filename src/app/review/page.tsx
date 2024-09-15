024a887f3d01b13a38913efd7dd2d517cbd8cb8b
FARUK2059 <omarfaruk34552@gmail.com>
2024-09-16 00:07:38 +0600

    Reciew section Complet

---
+++ src/app/review/page.tsx
@@ -0,0 +1,13 @@
+import Review from "@/components/ReviewSection/Review";
+
+
+
+const page = () => {
+    return (
+        <div>
+            <Review></Review>
+        </div>
+    );
+};
+
+export default page;
