e8607a6db9793b139fa38b349176d55e36207169
FARUK2059 <omarfaruk34552@gmail.com>
2024-09-16 00:24:23 +0600

    Compare Page Completed

---
+++ src/app/compare/page.tsx
@@ -0,0 +1,13 @@
+import Compare from "@/components/CompareSection/Compare";
+
+
+
+const page = () => {
+    return (
+        <div>
+            <Compare></Compare>
+        </div>
+    );
+};
+
+export default page;
